d2c1b474c02c73895ac4ec2c2816d33663ba86ef
rhyheart <8120183@qq.com>
2018-10-31 14:42:24 +0800

    add extension project.

---
+++ source/Demo.Extension/PersonExtension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Demo.Extension
+{
+    public static class PersonExtension
+    {
+        public static void Show(this Person person)
+        {
+            var desc = $"My name is {person.Name},I'm {person.Age} years old!";
+
+            Console.WriteLine(desc);
+        }
+    }
+}
---
+++ source/Demo.Extension/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Demo.Extension
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person
+            {
+                Name = "Rhy Heart",
+                Age = 23
+            };
+
+            person.Show();
+
+            Console.ReadKey();
+        }
+    }
+}

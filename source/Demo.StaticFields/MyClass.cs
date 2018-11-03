5048d721cc9212d6d3a5518c4e1bbaf8a461cc7e
恢雨 阮 <y8120183@outlook.com>
2018-11-03 11:52:42 +0800

    add static filed project

---
+++ source/Demo.StaticFields/MyClass.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Demo.StaticFields
+{
+    public class MyClass
+    {
+        public static string StaticFiled { set; get; }
+    }
+}
---
+++ source/Demo.StaticFields/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Demo.StaticFields
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("赋值前：" + MyClass.StaticFiled);
+            MyClass.StaticFiled = "Hello";
+            Console.WriteLine("赋值后：" + MyClass.StaticFiled);
+            MyClass.StaticFiled = "World";
+            Console.WriteLine("二次赋值：" + MyClass.StaticFiled);
+            Console.ReadKey();
+        }
+    }
+}

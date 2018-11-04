21f8e83f037df4586d74b0003040ac1c33a8f043
恢雨 阮 <y8120183@outlook.com>
2018-11-04 09:22:21 +0800

    add

---
+++ source/Demo.Enums/Color.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Demo.Enums
+{
+    public enum Color
+    {
+        Red,
+        Yellow,
+        Green
+    }
+}
---
+++ source/Demo.Enums/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Demo.Enums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var red = Color.Red;
+            Console.WriteLine("red:" + red + " " + (int)red);
+            var color = (Color)2;
+            Console.WriteLine("color:" + color + " " + (int)color);
+            Console.ReadKey();
+        }
+    }
+}

282d9c4d2bc5743d88edc47864fa3418f30b8144
rhyheart <8120183@qq.com>
2018-11-05 13:23:44 +0800

    add lambda project

---
+++ source/Demo.Lambdas/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Demo.Lambdas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = Operation(Sum);
+            Console.WriteLine("Func result is " + result);
+            var result2 = Operation((x, y) => x + y);
+            Console.WriteLine("Lambda result is " + result2);
+
+            Console.ReadKey();
+        }
+
+        static int Operation(Func<int, int, int> func)
+        {
+            int i = 3;
+            int j = 4;
+            return func.Invoke(i, j);
+        }
+
+        static int Sum(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}

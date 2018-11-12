6333b9f9e08ef7a3a1cd13ee680d1a1112313465
恢雨 阮 <y8120183@outlook.com>
2018-11-12 10:28:32 +0800

    add roche project

---
+++ source/Demo.Roche/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Demo.Roche
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test1();
+            Console.ReadKey();
+        }
+        
+        private static void Test1()
+        {
+            Console.WriteLine("Test1:");
+            var x = 1;
+            var y = 12;
+            var z = 123;
+            Console.WriteLine("x:{0:000}", x);
+            Console.WriteLine("y:{0:000}",y);
+            Console.WriteLine("z:{0:000}",z);
+        }
+    }
+}
